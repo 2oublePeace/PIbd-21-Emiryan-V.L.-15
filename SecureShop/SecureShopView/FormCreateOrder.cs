82835fdcb6ce05721ab4139fc39382cb97539568
2oublePeace <69040128+2oublePeace@users.noreply.github.com>
2021-04-05 14:58:23 +0400

    FormCreateOrder fix

--- SecureShop/SecureShopView/FormCreateOrder.cs
+++ SecureShop/SecureShopView/FormCreateOrder.cs
@@ -86,10 +86,11 @@ namespace SecureShopView
             {
                 _logicO.CreateOrder(new CreateOrderBindingModel
                 {
+                    EquipmentName = comboBoxEquipment.Text,
                     EquipmentId = Convert.ToInt32(comboBoxEquipment.SelectedValue),
                     Count = Convert.ToInt32(textBoxCount.Text),
                     Sum = Convert.ToDecimal(textBoxSum.Text)
-                });
+                });  
                 MessageBox.Show("Сохранение прошло успешно", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 DialogResult = DialogResult.OK;
                 Close();
