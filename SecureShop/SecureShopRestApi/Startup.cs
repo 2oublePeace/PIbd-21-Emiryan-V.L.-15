c128827837eaafa099b9edb297351d21e216605c
vova.emiryan <vova.emiryan@yandex.ru>
2021-05-17 12:57:06 +0400

    done

--- SecureShop/SecureShopRestApi/Startup.cs
+++ SecureShop/SecureShopRestApi/Startup.cs
@@ -10,10 +10,6 @@ using Microsoft.Extensions.Logging;
 using SecureShopBusinessLogic.BusinessLogics;
 using SecureShopBusinessLogic.Interfaces;
 using SecureShopDatabaseImplement.Implements;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace SecureShopRestApi
 {
